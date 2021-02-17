d8662fd8971d8bc5d244203b9cb3473b39575763
xarantolus <xarantolus@protonmail.com>
2021-02-17 11:53:06 +0100

    This implementation simply doesn't work

--- dyck.go
+++ dyck.go
@@ -1,58 +1,186 @@
 package jsonextract
 
-import "encoding/json"
+import (
+	"bufio"
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"strings"
+	"unicode/utf8"
+)
 
 const (
 	openObject = '{'
 	openArray  = '['
-
-	closeObject = '}'
-	closeArray  = ']'
 )
 
-var closing = map[rune]rune{
-	openObject: closeObject,
-	openArray:  closeArray,
+// StringObjects takes the given String and extracts all valid JSON objects / Arrays it can find
+func StringObjects(data string) (extracted []json.RawMessage) {
+	// Reader only returns errors if reading failed, which is not possible here
+	extracted, _ = ReaderObjects(strings.NewReader(data))
+	return
 }
 
-// String taks the given String and extracts all valid JSON objects / Arrays it can find
-func String(data string) (extracted []string) {
-	for i, r := range []rune(data) {
+func extract(text string, start int) json.RawMessage {
+	msg := json.RawMessage{}
+
+	err := json.Unmarshal([]byte(text[start:]), &msg)
+
+	if err == nil {
+		return msg
+	}
+
+	if syerr, ok := err.(*json.SyntaxError); ok {
+		fmt.Println(syerr.Offset)
+	}
+
+	return nil
+}
+
+// ReaderObjects takes the given io.Reader and reads all possible JSON objects it can find in it
+// Assumes the stream to consist of utf8 bytes
+func ReaderObjects(reader io.Reader) (objects []json.RawMessage, err error) {
+	// Need to buffer in order to be able to unread
+	buffered := prependableBuffer{
+		normalBuffer: bufio.NewReader(reader),
+	}
+
+	// decoder to decode our data
+	// If singleStepper wasn't there, the decoder would load too much
+	// data in its internal buffer, which would destroy the logic in the
+	// loop, as in it would read further than the JSON object
+	stepper := &singleStepper{r: buffered}
+	dec := json.NewDecoder(stepper)
+
+	for {
+		// stepper.Throwaway()
+
+		// Read every rune in our buffered stream
+		r, _, err := buffered.ReadRune()
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				// We are at the end of the reader
+				return objects, nil
+			}
+			return nil, err
+		}
+
+		// If we find any opening, we check if it continues with valid JSON data
 		if r == openObject || r == openArray {
-			closingPosition := nextBracket(data, r, closing[r], i)
-			if closingPosition == -1 {
+			// Before decoding, we step back one step to
+			// make sure the decoder will get the opening brace
+			err = buffered.UnreadRune() // TODO: Does this unread correctly?
+			if err != nil {
+				return nil, err
+			}
+
+			// we decode to json.RawMessage
+			var msg = json.RawMessage{}
+
+			err = dec.Decode(&msg)
+			if err != nil {
+				var prepend = stepper.Data(1)
+
+				buffered.Prepend(prepend)
 				continue
 			}
 
-			var roi = data[i:closingPosition]
-
-			if json.Valid([]byte(roi)) {
-				extracted = append(extracted, roi)
-			}
+			objects = append(objects, json.RawMessage(msg))
+			stepper.Throwaway()
 		}
 	}
+}
+
+type prependableBuffer struct {
+	bufBefore bytes.Buffer
+
+	normalBuffer *bufio.Reader
+}
+
+func (s *prependableBuffer) Read(p []byte) (n int, err error) {
+	if s.bufBefore.Len() != 0 {
+		n, err = s.bufBefore.Read(p)
+		if err == io.EOF {
+			err = nil
+		}
+	}
+
+	n2, err2 := s.normalBuffer.Read(p[n:])
+
+	return n + n2, err2
+}
+
+func (b *prependableBuffer) ReadRune() (r rune, size int, err error) {
+	r, size, err = b.bufBefore.ReadRune()
+	if err == nil {
+		return
+	}
+	return b.normalBuffer.ReadRune()
+}
+
+func (b *prependableBuffer) UnreadRune() (err error) {
+	err = b.bufBefore.UnreadRune()
+	if err == nil {
+		return
+	}
+
+	return b.normalBuffer.UnreadRune()
+}
+
+func (s *prependableBuffer) Prepend(p []byte) {
+	if s.bufBefore.Len() == 0 {
+		// cannot fail
+		_, _ = s.bufBefore.Write(p)
+		return
+	}
+
+	data := append(p, s.bufBefore.Bytes()...)
+	s.bufBefore.Reset()
+
+	s.bufBefore.Write(data)
+	return
+}
+
+type singleStepper struct {
+	r prependableBuffer
+
+	buf bytes.Buffer
+}
+
+func (s *singleStepper) Read(p []byte) (n int, err error) {
+	next, size, err := s.r.normalBuffer.ReadRune()
+	if err != nil {
+		return
+	}
+
+	if len(p) < size {
+		return 0, nil
+	}
+
+	written := utf8.EncodeRune(p, next)
+
+	// keep the rune
+	s.buf.Write(p[:written])
+
+	return written, nil
+}
+
+func (s *singleStepper) Data(offset int) (bytes []byte) {
+	len := s.buf.Len()
+	if len < offset {
+		return
+	}
+
+	bytes = make([]byte, len-offset)
+	copy(bytes, s.buf.Bytes()[offset:])
+
+	s.Throwaway()
 
 	return
 }
 
-func nextBracket(text string, open, close rune, start int) int {
-	if len(text) < start {
-		return -1
-	}
-
-	var level int
-
-	for i := start; i < len(text); i++ {
-		if rune(text[i]) == open {
-			level++
-		} else if rune(text[i]) == close {
-			level--
-
-			if level == 0 {
-				return i + 1
-			}
-		}
-	}
-
-	return -1
+func (s *singleStepper) Throwaway() {
+	s.buf.Reset()
 }
--- dyck_test.go
+++ dyck_test.go
@@ -1,6 +1,7 @@
 package jsonextract
 
 import (
+	"encoding/json"
 	"reflect"
 	"testing"
 )
@@ -8,19 +9,119 @@ import (
 func TestString(t *testing.T) {
 	tests := []struct {
 		arg  string
-		want []string
+		want []json.RawMessage
 	}{
 		{
+			`{{ "test": "a" } {}text[] in {}between{}`,
+			[]json.RawMessage{
+				[]byte(`{ "test": "a" }`),
+				[]byte(`{}`),
+				[]byte(`[]`),
+				[]byte(`{}`),
+				[]byte(`{}`),
+			},
+		},
+		{
+			`{}some {}text[] in {}between{}`,
+			[]json.RawMessage{
+				[]byte(`{}`),
+				[]byte(`{}`),
+				[]byte(`[]`),
+				[]byte(`{}`),
+				[]byte(`{}`),
+			},
+		},
+		{
+			`{}{}[]{}{}`,
+			[]json.RawMessage{
+				[]byte(`{}`),
+				[]byte(`{}`),
+				[]byte(`[]`),
+				[]byte(`{}`),
+				[]byte(`{}`),
+			},
+		},
+		{
 			`{"a": "b"}`,
-			[]string{`{"a": "b"}`},
+			[]json.RawMessage{[]byte(`{"a": "b"}`)},
 		},
 		{
 			"[1, 3, 55]",
-			[]string{"[1, 3, 55]"},
+			[]json.RawMessage{[]byte("[1, 3, 55]")},
 		},
 		{
 			"[1, 3, 55, ]",
 			nil,
+		},
+		{
+			`{
+  "login": "xarantolus",
+  "id": 0,
+  "node_id": "----",
+  "avatar_url": "https://avatars.githubusercontent.com/u/----",
+  "gravatar_id": "",
+  "url": "https://api.github.com/users/xarantolus",
+  "html_url": "https://github.com/xarantolus",
+  "followers_url": "https://api.github.com/users/xarantolus/followers",
+  "following_url": "https://api.github.com/users/xarantolus/following{/other_user}",
+  "gists_url": "https://api.github.com/users/xarantolus/gists{/gist_id}",
+  "starred_url": "https://api.github.com/users/xarantolus/starred{/owner}{/repo}",
+  "subscriptions_url": "https://api.github.com/users/xarantolus/subscriptions",
+  "organizations_url": "https://api.github.com/users/xarantolus/orgs",
+  "repos_url": "https://api.github.com/users/xarantolus/repos",
+  "events_url": "https://api.github.com/users/xarantolus/events{/privacy}",
+  "received_events_url": "https://api.github.com/users/xarantolus/received_events",
+  "type": "User",
+  "site_admin": false,
+  "name": "----",
+  "company": null,
+  "blog": "----",
+  "location": "----",
+  "email": "----",
+  "hireable": "----",
+  "bio": "----",
+  "twitter_username": null,
+  "public_repos": 17,
+  "public_gists": 3,
+  "followers": 13,
+  "following": 242,
+  "created_at": "2017-10-02T18:47:02Z",
+  "updated_at": "2021-01-08T20:42:33Z"
+}`,
+			[]json.RawMessage{[]byte(`{
+  "login": "xarantolus",
+  "id": 0,
+  "node_id": "----",
+  "avatar_url": "https://avatars.githubusercontent.com/u/----",
+  "gravatar_id": "",
+  "url": "https://api.github.com/users/xarantolus",
+  "html_url": "https://github.com/xarantolus",
+  "followers_url": "https://api.github.com/users/xarantolus/followers",
+  "following_url": "https://api.github.com/users/xarantolus/following{/other_user}",
+  "gists_url": "https://api.github.com/users/xarantolus/gists{/gist_id}",
+  "starred_url": "https://api.github.com/users/xarantolus/starred{/owner}{/repo}",
+  "subscriptions_url": "https://api.github.com/users/xarantolus/subscriptions",
+  "organizations_url": "https://api.github.com/users/xarantolus/orgs",
+  "repos_url": "https://api.github.com/users/xarantolus/repos",
+  "events_url": "https://api.github.com/users/xarantolus/events{/privacy}",
+  "received_events_url": "https://api.github.com/users/xarantolus/received_events",
+  "type": "User",
+  "site_admin": false,
+  "name": "----",
+  "company": null,
+  "blog": "----",
+  "location": "----",
+  "email": "----",
+  "hireable": "----",
+  "bio": "----",
+  "twitter_username": null,
+  "public_repos": 17,
+  "public_gists": 3,
+  "followers": 13,
+  "following": 242,
+  "created_at": "2017-10-02T18:47:02Z",
+  "updated_at": "2021-01-08T20:42:33Z"
+}`)},
 		},
 		{
 			"askdflaksmvalsd",
@@ -30,12 +131,50 @@ func TestString(t *testing.T) {
 			`"json encoded text\nNew line"`,
 			nil,
 		},
+		{
+			`{
+				"test": "this is a very }{} mean string"	
+			}`,
+			[]json.RawMessage{
+				json.RawMessage([]byte(`{
+				"test": "this is a very }{} mean string"	
+			}`)),
+			},
+		},
+		{
+			`{
+				"test": "this is another very ][] mean string"	
+			}`,
+			[]json.RawMessage{
+				[]byte(
+					`{
+				"test": "this is another very ][] mean string"	
+			}`),
+			},
+		},
+		{
+			`{}some {}text[] in {}between{}`,
+			[]json.RawMessage{
+				[]byte(`{}`),
+				[]byte(`{}`),
+				[]byte(`[]`),
+				[]byte(`{}`),
+				[]byte(`{}`),
+			},
+		},
 	}
 	for _, tt := range tests {
-		t.Run(t.Name(), func(t *testing.T) {
-			if gotExtracted := String(tt.arg); !reflect.DeepEqual(gotExtracted, tt.want) {
-				t.Errorf("String() = %v, want %v", gotExtracted, tt.want)
+		t.Run(t.Name()  , func(t *testing.T) {
+			if gotExtracted := StringObjects(tt.arg); !reflect.DeepEqual(gotExtracted, tt.want) {
+				t.Errorf("String() = %v, want %v", convert(gotExtracted), convert(tt.want))
 			}
 		})
 	}
+}
+
+func convert(m []json.RawMessage) (msgs []string) {
+	for _, v := range m {
+		msgs = append(msgs, string(v))
+	}
+	return
 }
